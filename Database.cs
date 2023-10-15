5cc4f110314203947907fc5a6dc7af89be79d6a7
pawel <pawel@DESKTOP>
2023-10-15 23:57:06 +0200

    Dodaj pliki projektów.

---
+++ Database.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Singleton;
+using static Singleton.Database;
+
+
+namespace Singleton {
+
+    public class Database {
+
+        private char[] tab = new char[100];
+        private static Database database;
+        private Database() {
+
+        }
+        private static Database GetInstance() {
+
+            if (database == null) {
+                database = new Database();
+            }
+            return database;
+        }
+        public static IConnection GetConnection() {
+
+            return Connection.GetInstance();
+        }
+
+
+
+
+
+        public class Connection : IConnection {
+
+            private static int i = 0;
+            private static Connection[] connectionsTab = {
+
+                new Connection(),
+                new Connection(),
+                new Connection()
+            };
+            private Connection() { 
+            
+            }
+            public static IConnection GetInstance() {
+
+                if (database == null) {
+                    database = Database.GetInstance();
+                }
+                if (i == connectionsTab.Length) {
+                    i = 0;
+                }
+                return connectionsTab[i++];
+            }
+            public char Get(int index) {
+
+                return database.tab[index];
+            }
+            public void Set(int index, char c) {
+
+                database.tab[index] = c;
+            }
+            public int Length() { 
+
+                return database.tab.Count();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Singleton {
+    class Program {
+        static void Main(string[] args) {
+
+            //  3 different connections to database:
+            IConnection connection1 = Database.GetConnection();
+            IConnection connection2 = Database.GetConnection();
+            IConnection connection3 = Database.GetConnection();
+
+            // Test of different connections:
+            Console.WriteLine("connection1 == connection2: " + (connection1 == connection2));
+            Console.WriteLine("connection2 == connection3: " + (connection2 == connection3));
+
+            // New con4 and test if con4 == con1:
+            IConnection connection4 = Database.GetConnection();
+            Console.WriteLine("connection1 == connection4: " + (connection1 == connection4));
+
+            // Test if con1 will give the same value as con2
+            connection1.Set(0, 'a');
+            connection2.Set(1, 'b');
+
+            Console.WriteLine("connection1.get(0) == connection2.get(0): " + (connection1.Get(0) == connection2.Get(0)));
+            Console.WriteLine("connection1.get(1) == connection2.get(1): " + (connection1.Get(1) == connection2.Get(1)));
+        }
+    }
+}
